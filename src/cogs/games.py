c03a04c321cd1855f5f32b80d19f4f60ba077d60
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-06-17 19:29:30 -0400

    added coin system, added balance
    shop coming soom

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -9,32 +9,35 @@ class Games(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def chance(self, ctx):
+    async def chance(self, ctx: commands.Context):
         """
         Try your luck at a chance game.
         If the emojis are the same, you win.
         """
-        # make a list of the possible symbols
-        emojis = [
-            ":cherries:",
-            ":banana:",
-            ":apple:",
-            ":grapes:",
-            ":tangerine:",
-            ":moneybag:",
-            ":gem:",
-            ":heart:",
-            ":clubs:",
-            ":spades:",
-        ]
+
+        # dictionary of the possible symbols and coins they represent
+        symbols = {
+            ":cherries:": 1,
+            ":banana:": 2,
+            ":apple:": 2,
+            ":grapes:": 3,
+            ":tangerine:": 2,
+            ":moneybag:": 6,
+            ":gem:": 8,
+            ":heart:": 10,
+            ":clubs:": 4,
+            ":spades:": 6,
+        }
+        emojis = list(symbols.keys())
 
         chance = random.randint(1, 100)
         win = chance <= 25
 
         if win:
             slot_1 = slot_2 = slot_3 = random.choice(emojis)
-            description = ":tada:" * 2 + "**You won!**" + ":tada:" * 2
+            title = ":tada:" * 2 + "**You won!**" + ":tada:" * 2
             color = 0x00FF00  # green
+            coins = symbols[slot_1]
         else:
             # make it so the slots don't match
             while True:
@@ -44,12 +47,22 @@ class Games(commands.Cog):
                 if slot_1 != slot_2 and slot_2 != slot_3 and slot_3 != slot_1:
                     break
 
-            description = "**You lost!**"
+            title = "**You lost!**"
             color = 0xFF0000  # red
+            coins = 0
+
+        coin_msg = (
+            f"You won {coins} coin" + ("s" if coins != 1 else "") + "!"
+            if coins != 0
+            else "You didn't win any coins."
+        )
+        if coins == 10:
+            coin_msg += "\nlove is the most expensive gift in the world."
 
         # set up the embed
         embed = discord.Embed(
-            description=description,
+            title=title,
+            description=coin_msg,
             color=color,
         )
 
@@ -68,6 +81,31 @@ class Games(commands.Cog):
             name="Slot 3",
             value=slot_3,
             inline=True,
+        )
+        # send the embed
+        await ctx.send(embed=embed)
+        # add the coins to the user's balance
+        await self.bot.db.players.update_one(
+            {"_id": ctx.author.id}, {"$inc": {"coins": coins}}, upsert=True
+        )
+
+    @commands.command()
+    async def balance(self, ctx: commands.Context):
+        """
+        Check your balance.
+        """
+
+        # get the user's balance
+        balance = await self.bot.db.players.find_one({"_id": ctx.author.id})
+        if balance is not None:
+            balance = balance["coins"]
+        else:
+            balance = 0
+        # set up the embed
+        embed = discord.Embed(
+            title=f"{ctx.author.name}'s balance",
+            description=f"{balance} coin" + ("s" if balance != 1 else ""),
+            color=0x00FF00,  # green
         )
         # send the embed
         await ctx.send(embed=embed)
