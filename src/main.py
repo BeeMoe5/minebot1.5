fbff664a2a7408d5dbcc57cf6d7dce66611d16f0
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-06-16 22:24:03 -0400

    added ping and echo commands

--- src/main.py
+++ src/main.py
@@ -4,8 +4,22 @@ import minebot
 from creds import TOKEN
 
 bot = minebot.MineBot(
-    command_prefix="m!", case_insensitive=True, intents=discord.Intents.all()
+    command_prefix="m!",
+    case_insensitive=True,
+    intents=discord.Intents.all(),
 )
+
+
+@bot.command()
+async def ping(ctx):
+    """Pong!"""
+    await ctx.send("Pong!")
+
+
+@bot.command(usage="<message>")
+async def echo(ctx, *, message: str):
+    """Echos a message."""
+    await ctx.send(message)
 
 
 if __name__ == "__main__":
