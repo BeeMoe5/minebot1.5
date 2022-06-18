4e25daefc0fc0112ca2bcc3fa6efdface9db26d2
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-06-17 21:19:13 -0400

    added shop and inventory commands

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -4,15 +4,23 @@ import discord
 from discord.ext import commands
 
 
-class Games(commands.Cog):
+class GamesAndEconomy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    def get_item(self, item_name):
+        for isle, isle_items in self.bot.shop_items.items():
+            for item in isle_items:
+                if item_name == item["name"]:
+                    return item
+        return None
 
     @commands.command()
     async def chance(self, ctx: commands.Context):
         """
         Try your luck at a chance game.
         If the emojis are the same, you win.
+        You will win as low as 1 and as high as 10 MineDollars.
         """
 
         # dictionary of the possible symbols and coins they represent
@@ -27,7 +35,7 @@ class Games(commands.Cog):
             ":heart:": 10,
             ":clubs:": 4,
             ":spades:": 6,
-        }
+        }  # type: dict[str, int]
         emojis = list(symbols.keys())
 
         chance = random.randint(1, 100)
@@ -37,7 +45,7 @@ class Games(commands.Cog):
             slot_1 = slot_2 = slot_3 = random.choice(emojis)
             title = ":tada:" * 2 + "**You won!**" + ":tada:" * 2
             color = 0x00FF00  # green
-            coins = symbols[slot_1]
+            coins: int = symbols[slot_1]
         else:
             # make it so the slots don't match
             while True:
@@ -49,12 +57,12 @@ class Games(commands.Cog):
 
             title = "**You lost!**"
             color = 0xFF0000  # red
-            coins = 0
+            coins: int = 0
 
         coin_msg = (
-            f"You won {coins} coin" + ("s" if coins != 1 else "") + "!"
+            f"You won {coins} MineDollar" + ("s" if coins != 1 else "") + "!"
             if coins != 0
-            else "You didn't win any coins."
+            else "You didn't win any MineDollars."
         )
         if coins == 10:
             coin_msg += "\nlove is the most expensive gift in the world."
@@ -86,10 +94,10 @@ class Games(commands.Cog):
         await ctx.send(embed=embed)
         # add the coins to the user's balance
         await self.bot.db.players.update_one(
-            {"_id": ctx.author.id}, {"$inc": {"coins": coins}}, upsert=True
+            {"_id": ctx.author.id}, {"$inc": {"coins": int(coins)}}, upsert=True
         )
 
-    @commands.command()
+    @commands.command(aliases=["bal"])
     async def balance(self, ctx: commands.Context):
         """
         Check your balance.
@@ -104,12 +112,136 @@ class Games(commands.Cog):
         # set up the embed
         embed = discord.Embed(
             title=f"{ctx.author.name}'s balance",
-            description=f"{balance} coin" + ("s" if balance != 1 else ""),
+            description=f"{balance} MineDollar" + ("s" if balance != 1 else ""),
             color=0x00FF00,  # green
         )
         # send the embed
         await ctx.send(embed=embed)
 
+    @commands.group()
+    async def shop(self, ctx: commands.Context):
+        """
+        Check the shop.
+        """
+
+        if ctx.invoked_subcommand is None:
+
+            # set up the embed
+            embed = discord.Embed(
+                title="Shop",
+                description="Here is the shop.",
+                color=0x00FF00,  # green
+            )
+            # add the items to the embed
+            for isle, isle_items in self.bot.shop_items.items():
+                items = [f"{item['name']} - {item['price']}" for item in isle_items]
+                embed.add_field(
+                    name=isle,
+                    value="\n".join(items),
+                    inline=False,
+                )
+            # send the embed
+            await ctx.send(embed=embed)
+
+    @shop.command(usage="<item>")
+    async def buy(self, ctx: commands.Context, item: str):
+        """
+        Buy an item from the shop.
+        """
+
+        # get the user's balance
+        balance = await self.bot.db.players.find_one({"_id": ctx.author.id})
+        if balance is not None:
+            balance = balance["coins"]
+        else:
+            return await ctx.send("You don't have any MineDollars.")
+        # get the item
+        item = self.get_item(item.lower())
+        if item is None:
+            return await ctx.send("That item isn't in the shop.")
+        # check if the user has enough money
+        if item["price"] > balance:
+            return await ctx.send("You don't have enough MineDollars.")
+        # subtract the price from the user's balance
+        await self.bot.db.players.update_one(
+            {"_id": ctx.author.id}, {"$inc": {"coins": int(-item["price"])}}
+        )
+        # add the item to the user's inventory
+        await self.bot.db.players.update_one(
+            {"_id": ctx.author.id}, {"$push": {"inventory": item["name"]}}, upsert=True
+        )
+        # send a message
+        await ctx.send(f"You bought {item['name']}!")
+
+    @commands.group(aliases=["inv"])
+    async def inventory(self, ctx: commands.Context):
+        """
+        Check your inventory.
+        """
+
+        if ctx.invoked_subcommand is None:
+
+            # get the user's inventory
+            inventory = await self.bot.db.players.find_one({"_id": ctx.author.id})
+            if inventory:
+                inventory = inventory["inventory"]
+
+            # set up the embed
+            embed = discord.Embed(
+                title=f"{ctx.author.name}'s inventory",
+                color=0x00FF00,  # green
+            )
+
+            if not inventory:
+                embed.description = "You don't have any items."
+                return await ctx.send(embed=embed)
+
+            items = {}
+            for item in inventory:
+                if item in items:
+                    items[item] += 1
+                else:
+                    items[item] = 1
+            # add the items to the embed
+            embed.description = ""
+            for n, item in enumerate(items.keys(), start=1):
+                embed.description += f"{n}. {item.title()} ({items[item]})\n"
+            # send the embed
+            await ctx.send(embed=embed)
+
+    @inventory.command(usage="<item>")
+    async def use(self, ctx: commands.Context, item: str):
+        """
+        Use an item from your inventory.
+        """
+
+        # get the user's inventory
+        inventory = await self.bot.db.players.find_one({"_id": ctx.author.id})
+        if inventory is not None:
+            inventory = inventory["inventory"]
+        else:
+            return await ctx.send("You don't have any items.")
+        # get the item
+        item = self.get_item(item.lower())
+        if item is None:
+            return await ctx.send("That item is invalid.")
+        # check if item in inventory
+        if item["name"] not in inventory:
+            return await ctx.send("You don't have that item.")
+        # check if the item is usable
+        if item["usable"] is False:
+            return await ctx.send("That item isn't usable.")
+        # use the item
+        # if the item is a food or drink, remove it from the inventory
+        if item["type"] in ["food", "drink"]:
+            await self.bot.db.players.update_one(
+                {"_id": ctx.author.id},
+                {"$pull": {"inventory": item["name"]}},
+            )
+
+        # send a message
+        await ctx.send(item["use_msg"])
+
 
 def setup(bot):
-    bot.add_cog(Games(bot))
+    bot.add_cog(GamesAndEconomy(bot))
--- src/minebot.py
+++ src/minebot.py
@@ -1,3 +1,4 @@
+import json
 import random
 from pathlib import Path
 
@@ -11,20 +12,24 @@ class MineBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.help_command = MineBotHelp()
-        self.is_loaded = False
+        self.cogs_loaded = False
+        self.db_connected = False
+        self.shop_loaded = False
+        self.shop_items = None
         self.db = None
 
     def load_cogs(self):
-        # get cog directory
-        cogs_dir = Path(__file__).parent / "cogs"
-        # get every file in the directory
-        for file in cogs_dir.iterdir():
-            # get the file name
-            file_name = file.name
-            # .py file verification
-            if file_name.endswith(".py"):
-                self.load_extension(f"cogs.{file_name[:-3]}")
-                print(f"Loaded {file_name}")
+        if not self.cogs_loaded:
+            # get cog directory
+            cogs_dir = Path(__file__).parent / "cogs"
+            # get every file in the directory
+            for file in cogs_dir.iterdir():
+                # get the file name
+                file_name = file.name
+                # .py file verification
+                if file_name.endswith(".py"):
+                    self.load_extension(f"cogs.{file_name[:-3]}")
+                    print(f"Loaded {file_name}")
 
     def connect_db(self):
         # connect to mongodb
@@ -32,17 +37,23 @@ class MineBot(commands.Bot):
             client = AsyncIOMotorClient("mongodb://localhost:27017/")
             self.db = client.minebot
 
+    def load_shop(self):
+        # get the shop items
+        if not self.shop_loaded:
+            with open(Path(__file__).parent / "shop.json", "r") as shop_file:
+                self.shop_items = json.load(shop_file)
+
     async def on_ready(self):
         print("Logged in as")
         print(self.user.name)
         print(self.user.id)
         print("------")
         # cog loading
-        if not self.is_loaded:
-            self.load_cogs()
-            self.is_loaded = True
+        self.load_cogs()
         # db connection
         self.connect_db()
+        # shop loading
+        self.load_shop()
 
     async def on_command_error(self, context, exception):
         if isinstance(exception, commands.CommandNotFound):
